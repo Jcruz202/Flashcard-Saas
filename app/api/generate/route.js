2c72ea12108d9869666cdf19115d19fb0c476ad6
Jcruz202 <Jcruz85@uic.edu>
2024-08-16 18:47:48 -0500

    cards flipping, ai works

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -17,6 +17,7 @@ When dealing with numerical data or dates, round to appropriate significant figu
 For language learning flashcards, include pronunciation guides where relevant.
 Use a consistent format for similar types of information across multiple flashcards.
 When creating flashcards for a series or set, ensure a logical progression of difficulty or complexity.
+Only generate 10 flashcards.
 
 Return in the following JSON format
 {
@@ -30,19 +31,20 @@ Return in the following JSON format
 `
 
 export async function POST(req) {
-    const openai = OpenAI()
+    const openai = new OpenAI()
     const data = await req.text()
 
-    const completion = await openai.chat.completion.create({
+    const completion = await openai.chat.completions.create({
         messages: [
-            {role: "system", content: systemPrompt},
-            {role: "user", content: data}
+            {role: 'system', content: systemPrompt},
+            {role: 'user', content: data}
         ],
-        model: "gpt-4o",
+        model: 'gpt-4o',
         response_format: {type: 'json_object'},
     })
+    // console.log(completion.choices[0].message.content)
 
     const flashcards = JSON.parse(completion.choices[0].message.content)
 
-    return NextResponse.json(flashcards.flashcard)
+    return NextResponse.json(flashcards.flashcards)
 }
